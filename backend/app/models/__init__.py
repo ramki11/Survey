5b53222524ab733e7739e7a5d28aa6f7b0b07936
jakethrasher <mjakethrasher@gmail.com>
2024-09-17 16:54:19 -0700

    break out model modules

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,21 +1,54 @@
-from .inquiry import Inquiry, InquiryPublic, InquiryCreate
+"""
+Survey models
+~~~~~~~~~~~~~
 
-from .deprecated_models import (
-    Item,
-    ItemCreate,
-    ItemUpdate,
-    ItemPublic,
-    ItemsPublic,
+Basic usage:
+    >>> from app.models import User, UserCreate
+
+Important:
+    Models that inherit from SQLModel and are configured with table=true, should be added as strings to __all__,
+    otherwise migrations will not work properly.
+
+    https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/?h=metadata#sqlmodel-metadata
+"""
+
+from .auth import Message, NewPassword, Token, TokenPayload, UpdatePassword
+from .inquiry import Inquiry, InquiryCreate, InquiryPublic
+from .item import Item, ItemCreate, ItemPublic, ItemsPublic, ItemUpdate
+from .user import (
     User,
     UserCreate,
-    UserUpdate,
     UserPublic,
-    UsersPublic,
     UserRegister,
+    UsersPublic,
+    UserUpdate,
     UserUpdateMe,
-    Message,
-    NewPassword,
-    UpdatePassword,
-    Token,
-    TokenPayload,
 )
+
+# https://realpython.com/python-all-attribute/#names-from-a-package
+__all__ = [
+    # auth model
+    "Message",
+    "NewPassword",
+    "Token",
+    "TokenPayload",
+    "UpdatePassword",
+    # inquiry model
+    "Inquiry",
+    "InquiryCreate",
+    "InquiryPublic",
+    # item model
+    "Item",
+    "ItemCreate",
+    "ItemPublic",
+    "ItemsPublic",
+    "ItemUpdate",
+    # user model
+    "User",
+    "UserCreate",
+    "UserPublic",
+    "UserRegister",
+    "UsersPublic",
+    "UserUpdate",
+    "UserUpdateMe",
+]
---
+++ backend/app/models/auth.py
@@ -0,0 +1,27 @@
+from sqlmodel import Field, SQLModel
+
+
+# Generic message
+class Message(SQLModel):
+    message: str
+
+
+# JSON payload containing access token
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+# Contents of JWT token
+class TokenPayload(SQLModel):
+    sub: str | None = None
+
+
+class NewPassword(SQLModel):
+    token: str
+    new_password: str = Field(min_length=8, max_length=40)
+
+
+class UpdatePassword(SQLModel):
+    current_password: str = Field(min_length=8, max_length=40)
+    new_password: str = Field(min_length=8, max_length=40)
