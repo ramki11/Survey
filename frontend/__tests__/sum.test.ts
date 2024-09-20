ff04c328d4514c8f1cc27d00711064335dd5e4e2
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-09-19 23:19:51 -0400

    mock service worker implemented

--- frontend/__tests__/sum.test.ts
+++ frontend/__tests__/sum.test.ts
@@ -1,8 +1,19 @@
 import { sum } from "../src/sum"
-import { describe, test, expect } from "@jest/globals"
+import { describe, test, expect, beforeAll } from "@jest/globals"
+import { server } from '../src/mocks/node.ts';
 
 describe('Sum function', () => {
     test('Returns correct value', () => {
         expect(sum(2, 3)).toEqual(5)
+    })
+})
+beforeAll(() => {
+    server.listen()
+})
+describe('API Test', () => {
+    test('Returns API', async () => {
+        const response = await fetch('http://localhost/api/v1/items')
+        const data = await response.json()
+        expect(data.firstName).toEqual('John')
     })
 })
--- frontend/src/client/index.ts
+++ frontend/src/client/index.ts
@@ -1,8 +1,3 @@
-if (process.env.NODE_ENV === 'development') {
-    const { worker } = require('./mocks/browser');
-    worker.start();
-}
-
 export { ApiError } from "./core/ApiError";
 export { CancelablePromise, CancelError } from "./core/CancelablePromise";
 export { OpenAPI } from "./core/OpenAPI";
