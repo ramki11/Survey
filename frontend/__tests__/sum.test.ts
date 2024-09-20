1e71f648987105cb67d74a97f093775d049831a2
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-09-20 00:37:02 -0400

    updated test

--- frontend/__tests__/sum.test.ts
+++ frontend/__tests__/sum.test.ts
@@ -2,14 +2,16 @@ import { sum } from "../src/sum"
 import { describe, test, expect, beforeAll } from "@jest/globals"
 import { server } from '../src/mocks/node.ts';
 
+beforeAll(() => {
+    server.listen()
+})
+
 describe('Sum function', () => {
     test('Returns correct value', () => {
         expect(sum(2, 3)).toEqual(5)
     })
 })
-beforeAll(() => {
-    server.listen()
-})
+
 describe('API Test', () => {
     test('Returns API', async () => {
         const response = await fetch('http://localhost/api/v1/items')
