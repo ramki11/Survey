0f5a107443588c8faa97957f9be3773f0924fc68
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-09-21 00:59:04 -0400

    updated inquiry class, added Response class

--- backend/app/models/inquiry.py
+++ backend/app/models/inquiry.py
@@ -1,7 +1,15 @@
 import uuid
 from datetime import datetime, timezone
+from typing import List, Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
+
+
+class Response(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    content: str = Field(min_length=1, max_length=255)
+    inquiry_id: uuid.UUID = Field(foreign_key="inquiry.id")
+    inquiry: Optional["Inquiry"] = Relationship(back_populates="responses")
 
 
 # Shared properties
@@ -9,19 +17,17 @@ class InquiryBase(SQLModel):
     text: str = Field(min_length=10, max_length=255)
 
 
-# Properties to receive on inquiry creation
 class InquiryCreate(InquiryBase):
     text: str = Field(min_length=10, max_length=255)
 
 
-# Database model, database table inferred from class name
 class Inquiry(InquiryBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     text: str = Field(min_length=10, max_length=255, unique=True)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    responses: List[Response] = Relationship(back_populates="inquiry")
 
 
-# Properties to return via API, id is always required
 class InquiryPublic(InquiryBase):
     id: uuid.UUID
     text: str
