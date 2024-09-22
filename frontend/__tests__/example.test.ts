983eba5c2a3865d595ae61bd51c2dce4c15feafa
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-09-21 20:08:54 -0400

    eslint disabled

--- frontend/__tests__/example.test.ts
+++ frontend/__tests__/example.test.ts
@@ -1,6 +1,6 @@
 import { http, HttpResponse } from "msw"
 import { setupServer } from "msw/node"
-
+/* eslint-disable */
 const inquiryURL = "http://localhost/api/v1/inquiries"
 
 const handlers = [
