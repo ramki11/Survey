c23a967738b142ea69906feae56651e4ee8477d7
mcverter <mitchell.verter@gmail.com>
2024-09-24 19:01:16 -0400

    Revert "removed and ignored tests"
    This reverts commit 4954b2411cd2d9ae8d8f41ab87ff1458a0e1e4db.

--- backend/app/tests/api/routes/test_login.py
+++ backend/app/tests/api/routes/test_login.py
@@ -30,19 +30,16 @@ def test_get_access_token_incorrect_password(client: TestClient) -> None:
     assert r.status_code == 400
 
 
-# Removed this test because it calls get_current_user
-#  which uses sqlAlchemy session instead of sqlModel session
-#
-# def test_use_access_token(
-#     client: TestClient, superuser_token_headers: dict[str, str]
-# ) -> None:
-#     r = client.post(
-#         f"{settings.API_V1_STR}/login/test-token",
-#         headers=superuser_token_headers,
-#     )
-#     result = r.json()
-#     assert r.status_code == 200
-#     assert "email" in result
+def test_use_access_token(
+    client: TestClient, superuser_token_headers: dict[str, str]
+) -> None:
+    r = client.post(
+        f"{settings.API_V1_STR}/login/test-token",
+        headers=superuser_token_headers,
+    )
+    result = r.json()
+    assert r.status_code == 200
+    assert "email" in result
 
 
 def test_recovery_password(
