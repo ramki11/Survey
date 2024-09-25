29dae32fa8c45f0024ebeb75c72a9d44e3b58b33
Neil Bae <31051480+ilhanbae@users.noreply.github.com>
2024-09-25 17:53:43 -0400

    test: Add f/e unit tests for displaying inquiries table (#61)
    # Task
    Add unit tests for listing inquiries.
    
    [Link to
    Ticket](https://incredihire-academy.atlassian.net/jira/software/c/projects/SR/boards/3?selectedIssue=SR-47)
    
    ## Author Tasks
    I added f/e unit tests for `InquiriesTable` component using jest and
    react-testing library.
    
    
    Some changes I had to make:
    * I had to change the payload type for Inquiry's `created_at` from Date
    to string, since it will be converted to JSON.
    * I had to move `formatDate` function to be declared inside
    `InquiriesTable` component, so that I can mock the behavior of
    `formatDate` in my test.
      * I mocked user's timezone as `America/Los_Angeles` for the test
      
    ## Acceptance Criteria
    - [x] should display empty table when there's no inquiries
    - [x] should display correct number of inquiries in table
    - [x] should display correct inquiry text
    - [x] should display correct inquiry created date and time, formatted to
    the user's timzeone
    - [x] should display "Invalid Date" for inquiries with invalid created
    date and time
    - [x] should display inquiries from newest to oldest
    - [x] should log the inquiry details on console when clicked

--- frontend/src/components/Inquiries/InquiriesTable.tsx
+++ frontend/src/components/Inquiries/InquiriesTable.tsx
@@ -1,86 +1,105 @@
-import {useQuery} from "@tanstack/react-query";
-import {SkeletonText, Table, TableContainer, Tbody, Td, Th, Thead, Tr} from "@chakra-ui/react";
-import * as InquiriesService from "../../client/services/inquiriesService.ts";
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
+import {
+  SkeletonText,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react"
+import { useQuery } from "@tanstack/react-query"
+import dayjs from "dayjs"
+import timezone from "dayjs/plugin/timezone"
+import utc from "dayjs/plugin/utc"
+import { useMemo } from "react"
+import * as InquiriesService from "../../client/services/inquiriesService.ts"
 
 // Dayjs Configurations
-// eslint-disable-next-line
-dayjs.extend(utc);
-// eslint-disable-next-line
-dayjs.extend(timezone);
-// eslint-disable-next-line
-const userTimezone = dayjs.tz.guess();
+/* eslint-disable */
+dayjs.extend(utc)
+dayjs.extend(timezone)
+/* eslint-enable */
 
-function getInquiriesQueryOptions() {
+const InquiriesTable = () => {
+  // Format ISO date to the user's timezone.
+  // ex. Sep 17, 2024 14:13 PM
+  function formatDate(date: string): string {
+    const invalidDateMessage = "Invalid Date"
+    if (typeof date !== "string") return invalidDateMessage
+    // Unsafe access to 'error' typed value is handled by dayjs' isValid function
+    /* eslint-disable */
+    const parsedDate = dayjs.utc(date)
+    const userTimezone = dayjs.tz.guess()
+    return parsedDate.isValid()
+      ? parsedDate.tz(userTimezone).format("MMM DD, YYYY hh:mm A")
+      : invalidDateMessage
+    /* eslint-enable */
+  }
+
+  function getInquiriesQueryOptions() {
     return {
-        queryKey: ["inquiries"],
-        queryFn: () => InquiriesService.readInquiries(),
-    };
-}
-
-// Format date to user's timezone
-// ex. Sep 17, 2024 14:13 PM
-function formatDate(date: Date): string {
-    try {
-        // eslint-disable-next-line
-        return dayjs.utc(date).tz(userTimezone).format("MMM DD, YYYY hh:mm A");
-    } catch (error) {
-        console.error("Error formatting date:", error);
-        return new Date(date).toISOString();
+      queryKey: ["inquiries"],
+      queryFn: () => InquiriesService.readInquiries(),
     }
+  }
+
+  const { data: inquiries, isPending } = useQuery({
+    ...getInquiriesQueryOptions(),
+  })
+
+  // Sort inquiries from Newest to oldest
+  const sortedInquiries = useMemo(() => {
+    if (!inquiries?.data) return []
+    return inquiries.data.sort((a, b) => {
+      return new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+    })
+  }, [inquiries])
+
+  return (
+    <TableContainer>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Text</Th>
+            <Th>Created At</Th>
+          </Tr>
+        </Thead>
+        {isPending ? (
+          <Tbody>
+            <Tr>
+              {new Array(3).fill(null).map((_, index) => (
+                <Td key={index}>
+                  <SkeletonText noOfLines={1} />
+                </Td>
+              ))}
+            </Tr>
+          </Tbody>
+        ) : (
+          <Tbody>
+            {sortedInquiries.length > 0 ? (
+              sortedInquiries.map((inquiry) => (
+                <Tr
+                  key={inquiry.id}
+                  onClick={() => {
+                    console.log(inquiry)
+                  }}
+                  data-testid="inquiry-row"
+                >
+                  <Td data-testid="inquiry-text">{inquiry.text}</Td>
+                  <Td data-testid="inquiry-datetime">
+                    {formatDate(inquiry.created_at)}
+                  </Td>
+                </Tr>
+              ))
+            ) : (
+              <></>
+            )}
+          </Tbody>
+        )}
+      </Table>
+    </TableContainer>
+  )
 }
 
-function InquiriesTable() {
-    const { data: inquiries, isPending } = useQuery({
-        ...getInquiriesQueryOptions(),
-    });
-
-    // Sort inquries from Newest to oldest
-    const sortedInquiries = inquiries?.data.sort((a, b) => {
-        return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
-    });
-
-    return (
-        <>
-            <TableContainer>
-                <Table>
-                    <Thead>
-                        <Tr>
-                            <Th>Text</Th>
-                            <Th>Created At</Th>
-                        </Tr>
-                    </Thead>
-                    {isPending ? (
-                        <Tbody>
-                            <Tr>
-                                {new Array(3).fill(null).map((_, index) => (
-                                    <Td key={index}>
-                                        <SkeletonText noOfLines={1} />
-                                    </Td>
-                                ))}
-                            </Tr>
-                        </Tbody>
-                    ) : (
-                        <Tbody>
-                            {sortedInquiries?.map((inquiry) => (
-                                <Tr
-                                    key={inquiry.id}
-                                    onClick={() => {
-                                        console.log(inquiry);
-                                    }}
-                                >
-                                    <Td>{inquiry.text}</Td>
-                                    <Td>{formatDate(inquiry.created_at)}</Td>
-                                </Tr>
-                            ))}
-                        </Tbody>
-                    )}
-                </Table>
-            </TableContainer>
-        </>
-    );
-}
-
-export default InquiriesTable;
+export default InquiriesTable
