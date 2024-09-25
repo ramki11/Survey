505b9dca533b90b9d6d947ce345985c7697f7362
mcverter <mitchell.verter@gmail.com>
2024-09-24 21:40:50 -0400

    add uuids to sqlite to enable get_current_user

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -1,19 +1,21 @@
-# This app injects an in-memory database for testing https://fastapi.tiangolo.com/advanced/testing-database/#file-structure
-# Because some Sessions call "exec()", they have to use sqlmodel rather than sqlalchemy https://github.com/fastapi/sqlmodel/issues/75#issuecomment-2109911909
-# Sqlmodel represents UUIDs as hex but sqlalchemy represents them as plain strings.
-# get_current_user seems to employ sqlalchemy instead of sqlmodel so we might need to mock that for future tests.
+import uuid
+from http.client import HTTPException
 
-from collections.abc import Generator
-
+import jwt
 import pytest
+from cfgv import ValidationError
 from fastapi.testclient import TestClient
+from jwt import InvalidTokenError
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
+from starlette import status
 
-from app.api.deps import get_db
+from app.api.deps import SessionDep, TokenDep, get_current_user, get_db
+from app.core import security
 from app.core.config import settings
 from app.core.db import init_db
 from app.main import app
+from app.models import TokenPayload, User
 from app.tests.utils.user import authentication_token_from_email
 from app.tests.utils.utils import get_superuser_token_headers
 
@@ -40,48 +42,6 @@ def client_fixture(db: Session):
     app.dependency_overrides.clear()
 
 
-# @pytest.fixture(scope="module")
-# def client() -> Generator[TestClient, None, None]:
-#     with TestClient(app) as c:
-#         yield c
-#
-
-#
-# engine = create_engine(
-#     "sqlite:///:memory:",
-#     connect_args={"check_same_thread": False},
-#     poolclass=StaticPool,
-# )
-#
-# TestingSessionLocal = sessionmaker(
-#     class_=Session, autocommit=False, autoflush=False, bind=engine
-# )
-# SQLModel.metadata.create_all(engine)
-#
-#
-# def override_get_db() -> Generator[Session, None, None]:
-#     try:
-#         db = TestingSessionLocal()
-#         yield db
-#     finally:
-#         db.close()
-#
-#
-# app.dependency_overrides[get_db] = override_get_db
-#
-#
-# @pytest.fixture(scope="session", autouse=True)
-# def db() -> Generator[Session, None, None]:
-#     with Session(engine) as session:
-#         init_db(session)
-#         yield session
-#         statement = delete(Item)
-#         session.execute(statement)
-#         statement = delete(User)
-#         session.execute(statement)
-#         session.commit()
-
-
 @pytest.fixture(scope="module")
 def superuser_token_headers(client: TestClient) -> dict[str, str]:
     return get_superuser_token_headers(client)
@@ -92,3 +52,26 @@ def normal_user_token_headers(client: TestClient, db: Session) -> dict[str, str]
     return authentication_token_from_email(
         client=client, email=settings.EMAIL_TEST_USER, db=db
     )
+
+
+# sqlite does not have a native UUID type but our users do so we need to override get_current_user
+def get_current_user_force_uuid(session: SessionDep, token: TokenDep) -> User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+    except (InvalidTokenError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = session.get(User, uuid.UUID(token_data.sub))
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return user
+
+
+app.dependency_overrides[get_current_user] = get_current_user_force_uuid
