b1458a0952d71604cb05e91e0e895d5de1b1783c
Neil Bae <nbae0127@gmail.com>
2024-09-25 22:44:23 -0400

    Add get inquiry service tests

--- backend/app/tests/services/inquiry/test_get_inquiry_service.py
+++ backend/app/tests/services/inquiry/test_get_inquiry_service.py
@@ -1,26 +1,45 @@
+import uuid
+
 import pytest
 from sqlmodel import Session
 
 from app.models import Inquiry
 from app.services.inquiries import get_inquiry_by_id
 
-# Naming Convention:
-# <FUT>_when<Scenario>_should<Outcome>
-# ex. addTwoNumbers_whenOneNumberIsZero_shouldReturnTheSecondNumber
 
-
-@pytest.fixture(scope="module")
-def setup_data(db: Session) -> Inquiry:
+@pytest.fixture(name="single_inquiry", scope="module")
+def fixture_single_inquiry(db: Session) -> Inquiry:
     single_inquiry = Inquiry(text="How's your work-life balance?")
     db.add(single_inquiry)
     db.commit()
     return single_inquiry
 
 
-def getSingleInquiryService_whenCalledWithValidId_shouldReturnMatchingInquiry(
-    db: Session, sample_inquiry: Inquiry
+def test_getInquiryService_whenCalledWithInvalidId_shouldRaiseException(
+    db: Session,
 ) -> None:
-    result = get_inquiry_by_id(session=db, inquiry_id=sample_inquiry.id)
+    with pytest.raises(ValueError, match=r"Id must be a valid UUID"):
+        get_inquiry_by_id(session=db, inquiry_id=12345)
+
+    with pytest.raises(ValueError, match=r"Id must be a valid UUID"):
+        get_inquiry_by_id(session=db, inquiry_id="12345")
+
+    with pytest.raises(ValueError, match=r"Id must be a valid UUID"):
+        get_inquiry_by_id(session=db, inquiry_id="id")
+
+
+def test_getInquiryService_whenCalledWithExistentId_shouldReturnMatchingInquiry(
+    db: Session, single_inquiry: Inquiry
+) -> None:
+    result = get_inquiry_by_id(session=db, inquiry_id=single_inquiry.id)
     assert result is not None
-    assert result.id == sample_inquiry.id
-    assert result.text == sample_inquiry.text
+    assert result.id == single_inquiry.id
+    assert result.text == single_inquiry.text
+
+
+def test_getInquiryService_whenCalledWithNonExistentId_shouldReturnNone(
+    db: Session,
+) -> None:
+    random_uuid = uuid.uuid4()
+    result = get_inquiry_by_id(session=db, inquiry_id=random_uuid)
+    assert result is None
