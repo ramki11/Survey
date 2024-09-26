fa5ff68eb66e2420f7c404b00aa141d6db4574e4
Neil Bae <nbae0127@gmail.com>
2024-09-26 16:08:38 -0400

    Add get_inquiry api test

--- backend/app/tests/api/routes/inquiry/test_get_inquiry_api.py
+++ backend/app/tests/api/routes/inquiry/test_get_inquiry_api.py
@@ -1,71 +1,64 @@
-# import uuid
+import uuid
 
-# from fastapi.encoders import jsonable_encoder
-# from fastapi.testclient import TestClient
-# from sqlmodel import Session
+import pytest
+from fastapi.testclient import TestClient
+from sqlmodel import Session
 
-# from app.core.config import settings
-# from app.tests.utils.inquiry import create_random_inquiry
-
-
-# def test_read_inquries(
-#     client: TestClient, superuser_token_headers: dict[str, str], db: Session
-# ) -> None:
-#     first_response = client.get(
-#         f"{settings.API_V1_STR}/inquiries/",
-#         headers=superuser_token_headers,
-#     )
-#     assert first_response.status_code == 200
-
-#     initial_content = first_response.json()
-#     initial_inquiries = initial_content["data"]
-#     initial_length = len(initial_inquiries)
-
-#     # Create two new inquiries
-#     inquiry_1 = jsonable_encoder(create_random_inquiry(db))
-#     inquiry_2 = jsonable_encoder(create_random_inquiry(db))
-
-#     second_response = client.get(
-#         f"{settings.API_V1_STR}/inquiries/",
-#         headers=superuser_token_headers,
-#     )
-#     assert second_response.status_code == 200
-
-#     new_content = second_response.json()
-#     new_inquiries = new_content["data"]
-#     new_length = len(new_inquiries)
-
-#     assert new_length == initial_length + 2
-
-#     # Ensure the new inquiries are included in the new response
-#     assert any(inquiry["id"] == inquiry_1["id"] for inquiry in new_inquiries)
-#     assert any(inquiry["id"] == inquiry_2["id"] for inquiry in new_inquiries)
-
-#     # Ensure the new inquiries have the correct text
-#     assert any(inquiry["text"] == inquiry_1["text"] for inquiry in new_inquiries)
-#     assert any(inquiry["text"] == inquiry_2["text"] for inquiry in new_inquiries)
+from app.core.config import settings
+from app.models import Inquiry
 
 
-# def test_read_inquiry(
-#     client: TestClient, superuser_token_headers: dict[str, str], db: Session
-# ) -> None:
-#     inquiry = create_random_inquiry(db)
-#     response = client.get(
-#         f"{settings.API_V1_STR}/inquiries/{inquiry.id}",
-#         headers=superuser_token_headers,
-#     )
-#     assert response.status_code == 200
-#     content = response.json()
-#     assert content == jsonable_encoder(inquiry)
+@pytest.fixture(name="single_inquiry", scope="function")
+def fixture_single_inquiry(db: Session) -> Inquiry:
+    single_inquiry = Inquiry(text="How's your work-life balance?")
+    db.add(single_inquiry)
+    db.commit()
+    return single_inquiry
 
 
-# def test_read_inquiry_not_found(
-#     client: TestClient, superuser_token_headers: dict[str, str]
-# ) -> None:
-#     response = client.get(
-#         f"{settings.API_V1_STR}/inquiries/{uuid.uuid4()}",
-#         headers=superuser_token_headers,
-#     )
-#     assert response.status_code == 404
-#     content = response.json()
-#     assert content["detail"] == "Inquiry not found"
+def test_getInquiryAPI_whenCalledWithExistentId_shouldReturnInquiry(
+    client: TestClient, superuser_token_headers: dict[str, str], single_inquiry: Inquiry
+) -> None:
+    response = client.get(
+        f"{settings.API_V1_STR}/inquiries/{single_inquiry.id}",
+        headers=superuser_token_headers,
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert content["id"] == str(single_inquiry.id)
+    assert content["text"] == single_inquiry.text
+
+
+def test_getInquiryAPI_whenCalledWithNonExistentId_shouldReturnNotFound(
+    client: TestClient, superuser_token_headers: dict[str, str]
+) -> None:
+    random_uuid = uuid.uuid4()
+    response = client.get(
+        f"{settings.API_V1_STR}/inquiries/{random_uuid}",
+        headers=superuser_token_headers,
+    )
+    assert response.status_code == 404
+    content = response.json()
+    assert content["detail"] == "Inquiry not found"
+
+
+def test_getInquiryAPI_whenCalledWithInvalidIdType_shouldReturnBadRequest(
+    client: TestClient, superuser_token_headers: dict[str, str]
+) -> None:
+    id = "12345"
+    response = client.get(
+        f"{settings.API_V1_STR}/inquiries/{id}",
+        headers=superuser_token_headers,
+    )
+    assert response.status_code == 422
+    content = response.json()
+    assert "detail" in content
+
+    id = 12345
+    response = client.get(
+        f"{settings.API_V1_STR}/inquiries/{id}",
+        headers=superuser_token_headers,
+    )
+    assert response.status_code == 422
+    content = response.json()
+    assert "detail" in content
