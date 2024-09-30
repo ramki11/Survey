a41a5042a0308e255a0817c5e050cec77dcdd5ab
Neil Bae <nbae0127@gmail.com>
2024-09-30 18:00:17 -0400

    Rename error.ts to showToastOnError.ts

--- frontend/src/utils/showToastOnError.ts
+++ frontend/src/utils/showToastOnError.ts
@@ -1,31 +1,10 @@
 import type { ApiError } from "../client"
 
-interface ErrorDetail {
-  msg: string
-}
-
-interface ApiErrorBody {
-  detail?: string | ErrorDetail[]
-}
-
-// These variables are not used, they are just type definition
-/* eslint-disable */
-type ShowToastFunction = (
-  title: string,
-  description: string,
-  status: "success" | "error",
-) => void
-/* eslint-enable */
-
-export const handleError = (err: ApiError, showToast: ShowToastFunction) => {
-  const errDetail = (err.body as ApiErrorBody).detail
-  let errorMessage = "Something went wrong."
-  if (errDetail) {
-    if (typeof errDetail === "string") {
-      errorMessage = errDetail
-    } else if (Array.isArray(errDetail) && errDetail.length > 0) {
-      errorMessage = errDetail[0].msg
-    }
+export const handleError = (err: ApiError, showToast: any) => {
+  const errDetail = (err.body as any)?.detail
+  let errorMessage = errDetail || "Something went wrong."
+  if (Array.isArray(errDetail) && errDetail.length > 0) {
+    errorMessage = errDetail[0].msg
   }
   showToast("Error", errorMessage, "error")
 }
