59e54981d8cef18747765dec38947a0787a67e99
jakethrasher <mjakethrasher@gmail.com>
2024-09-30 21:11:22 -0700

    Update test function names

--- backend/app/tests/api/routes/scheduled_inquiry/test_post_scheduled_inquiry_api.py
+++ backend/app/tests/api/routes/scheduled_inquiry/test_post_scheduled_inquiry_api.py
@@ -8,7 +8,7 @@ from app.models.scheduled_inquiry import ScheduledInquiryPublic
 from app.services import inquiries as inquiries_service
 
 
-def test_create_scheduled_inquiry_when_called_with_inquiry_id_that_doesnt_exist_should_return_400(
+def test_post_scheduled_inquiry_when_called_with_inquiry_id_that_doesnt_exist_should_return_400(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
     invalid_inquiry_id = "f4d24b3e-a1af-437d-8dc4-2d50a7608725"
@@ -23,10 +23,10 @@ def test_create_scheduled_inquiry_when_called_with_inquiry_id_that_doesnt_exist_
     assert response.status_code == 400
 
 
-def test_create_scheduled_inquiry_when_called_with_inquiry_id_that_exists_should_return_200(
+def test_post_scheduled_inquiry_when_called_with_inquiry_id_that_exists_should_return_scheduled_inquiry(
     client: TestClient, superuser_token_headers: dict[str, str], db: Session
 ) -> None:
-    inquiry_in = InquiryCreate(text="I am a test inquiry")
+    inquiry_in = InquiryCreate(text="Testing the happy path")
     db_inquiry = inquiries_service.create_inquiry(session=db, inquiry_in=inquiry_in)
     test_dto = {"inquiry_id": str(db_inquiry.id)}
 
