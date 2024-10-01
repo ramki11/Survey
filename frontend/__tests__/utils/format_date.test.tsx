30c8ee3f39eed2280d3663375041c0c5d8fd615b
Neil Bae <nbae0127@gmail.com>
2024-10-01 14:35:58 -0400

    Add timezone tests

--- frontend/__tests__/utils/format_date.test.tsx
+++ frontend/__tests__/utils/format_date.test.tsx
@@ -1,16 +1,47 @@
 import { formatDate } from "../../src/utils"
-// import dayjs from "dayjs";
-// jest.spyOn(dayjs.tz, "guess").mockReturnValue("America/Los_Angeles")
+import "@testing-library/jest-dom"
+import dayjs from "dayjs"
+
+const timezoneTestData = {
+  input: "2024-09-23T12:00:00Z",
+  timezones: [
+    {
+      timezone: "America/New_York", // Eastern Daylight Time (EDT)
+      expected: "Sep 23, 2024 08:00 AM",
+    },
+    {
+      timezone: "America/Chicago", // Central Daylight Time (CDT)
+      expected: "Sep 23, 2024 07:00 AM",
+    },
+    {
+      timezone: "America/Denver", // Mountain Daylight Time (MDT)
+      expected: "Sep 23, 2024 06:00 AM",
+    },
+    {
+      timezone: "America/Los_Angeles", // Pacific Daylight Time (PDT)
+      expected: "Sep 23, 2024 05:00 AM",
+    },
+    {
+      timezone: "America/Anchorage", // Alaska Daylight Time (AKDT)
+      expected: "Sep 23, 2024 04:00 AM",
+    },
+    {
+      timezone: "Pacific/Honolulu", // Hawaii-Aleutian Standard Time (HAST, no DST)
+      expected: "Sep 23, 2024 02:00 AM",
+    },
+  ],
+}
 
 describe("formatDate", () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
-  it("should format valid ISO date string correctly", () => {
-    const input = "2024-09-23T12:00:00Z"
-    const expectedOutput = "Sep 23, 2024 08:00 AM"
-    expect(formatDate(input)).toBe(expectedOutput)
+  it("should format valid ISO date string correctly for different timezones", () => {
+    for (const { timezone, expected } of timezoneTestData.timezones) {
+      jest.spyOn(dayjs.tz, "guess").mockReturnValue(timezone)
+      expect(formatDate(timezoneTestData.input)).toBe(expected)
+    }
   })
 
   it("should return 'Invalid Date' for invalid ISO date string", () => {
