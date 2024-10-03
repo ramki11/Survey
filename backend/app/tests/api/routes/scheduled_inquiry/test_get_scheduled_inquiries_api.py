01959d75055de49f9fc099b62461b0d2564e8fa4
Jake Thrasher <mjakethrasher@gmail.com>
2024-10-03 16:41:07 -0700

    Add test for empty list

--- backend/app/tests/api/routes/scheduled_inquiry/test_get_scheduled_inquiries_api.py
+++ backend/app/tests/api/routes/scheduled_inquiry/test_get_scheduled_inquiries_api.py
@@ -1,60 +1,48 @@
-from collections.abc import Generator
-
-import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import Session
 
 from app.core.config import settings
-from app.models import Inquiry, InquiryCreate
+from app.models import InquiryCreate
 from app.services import inquiries as inquiries_service
 from app.services import scheduled_inquiries as scheduled_inquiries_service
 
-route_prefix = "scheduledinquiries"
-
-
-# Populate the db with two test inquiries
-@pytest.fixture
-def db_inquiries(db: Session) -> Generator[tuple[Inquiry, Inquiry]]:
-    # Setup
-    inquiry_in_1 = InquiryCreate(text="Test inquiry 1.")
-    inquiry_in_2 = InquiryCreate(text="Test inquiry 2.")
-    db_inquiry_1 = inquiries_service.create_inquiry(session=db, inquiry_in=inquiry_in_1)
-    db_inquiry_2 = inquiries_service.create_inquiry(session=db, inquiry_in=inquiry_in_2)
-
-    yield db_inquiry_1, db_inquiry_2
-
-    # Teardown
-    db.delete(db_inquiry_1)
-    db.delete(db_inquiry_2)
+path = f"{settings.API_V1_STR}/scheduledinquiries/"
 
 
 def test_get_scheduled_inquiries_when_scheduled_inquiries_exist_should_return_all(
     client: TestClient,
     superuser_token_headers: dict[str, str],
     db: Session,
-    db_inquiries: tuple[Inquiry, Inquiry],
 ) -> None:
-    initial_response = client.get(
-        f"{settings.API_V1_STR}/{route_prefix}/",
-        headers=superuser_token_headers,
-    )
+    # Setup
+    inquiry_in_1 = InquiryCreate(text="Test inquiry 1.")
+    inquiry_in_2 = InquiryCreate(text="Test inquiry 2.")
+    db_inquiry_1 = inquiries_service.create_inquiry(session=db, inquiry_in=inquiry_in_1)
+    db_inquiry_2 = inquiries_service.create_inquiry(session=db, inquiry_in=inquiry_in_2)
 
-    assert initial_response.status_code == 200
+    response = client.get(path, headers=superuser_token_headers)
 
-    initial_content = initial_response.json()
-    initial_length = initial_content["count"]
+    json = response.json()
+    initial_count = json["count"]
 
-    scheduled_inquiries_service.create(session=db, inquiry_id=db_inquiries[0].id)
-    scheduled_inquiries_service.create(session=db, inquiry_id=db_inquiries[1].id)
+    scheduled_inquiries_service.create(session=db, inquiry_id=db_inquiry_1.id)
+    scheduled_inquiries_service.create(session=db, inquiry_id=db_inquiry_2.id)
 
-    second_response = client.get(
-        f"{settings.API_V1_STR}/{route_prefix}/",
-        headers=superuser_token_headers,
-    )
+    response = client.get(path, headers=superuser_token_headers)
 
-    assert second_response.status_code == 200
+    json = response.json()
+    new_count = json["count"]
 
-    new_content = second_response.json()
-    new_length = new_content["count"]
+    assert new_count == initial_count + 2
 
-    assert new_length == initial_length + 2
+
+def test_get_scheduled_inquiries_when_empty_returns_empty_list(
+    client: TestClient,
+    superuser_token_headers: dict[str, str],
+) -> None:
+    response = client.get(path, headers=superuser_token_headers)
+
+    json = response.json()
+
+    assert len(json["data"]) == 0
+    assert json["count"] == 0
