5b3787c6aabdc59544970ea99b11e12aa709f9f5
Jake Thrasher <mjakethrasher@gmail.com>
2024-10-04 19:42:17 -0700

    Removed Items code

--- frontend/src/components/Common/DeleteAlert.tsx
+++ frontend/src/components/Common/DeleteAlert.tsx
@@ -11,7 +11,6 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 import React from "react"
 import { useForm } from "react-hook-form"
 
-import * as ItemsService from "../../client/services/itemsService"
 import * as UsersService from "../../client/services/usersService"
 import useCustomToast from "../../hooks/useCustomToast"
 
@@ -32,9 +31,7 @@ const Delete = ({ type, id, isOpen, onClose }: DeleteProps) => {
   } = useForm()
 
   const deleteEntity = async (id: string) => {
-    if (type === "Item") {
-      await ItemsService.deleteItem({ id: id })
-    } else if (type === "User") {
+    if (type === "User") {
       await UsersService.deleteUser({ userId: id })
     } else {
       throw new Error(`Unexpected type: ${type}`)
@@ -60,7 +57,7 @@ const Delete = ({ type, id, isOpen, onClose }: DeleteProps) => {
     },
     onSettled: () => {
       queryClient.invalidateQueries({
-        queryKey: [type === "Item" ? "items" : "users"],
+        queryKey: ["users"],
       })
     },
   })
