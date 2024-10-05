189f09bbe63c34b2d09bf356f0b1bbfe08e9ca5e
Jake Thrasher <mjakethrasher@gmail.com>
2024-10-04 19:38:56 -0700

    Remove all Items code from frontend and backend

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 
 from app.api.routes import (
     inquiries,
-    items,
     login,
     schedule,
     scheduled_inquiries,
@@ -17,7 +16,6 @@ api_router.include_router(inquiries.router, prefix="/inquiries", tags=["inquirie
 api_router.include_router(themes.router, prefix="/themes", tags=["themes"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(
     scheduled_inquiries.router,
     prefix="/scheduledinquiries",
