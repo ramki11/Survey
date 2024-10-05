58036c8fed4972aba905d65093832bfdfd09bfb9
Jake Thrasher <mjakethrasher@gmail.com>
2024-10-04 18:25:38 -0700

    Add context to revision name

--- backend/app/alembic/versions/6a5369b7e7a8_merge_heads.py
+++ backend/app/alembic/versions/6a5369b7e7a8_merge_heads.py
@@ -6,10 +6,6 @@ Create Date: 2024-10-04 17:21:17.205510
 
 """
 
-import sqlalchemy as sa
-import sqlmodel.sql.sqltypes
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision = "6a5369b7e7a8"
 down_revision = ("06210b2836d5", "99c1c47b8606")
