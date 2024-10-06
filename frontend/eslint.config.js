c081e690a949edb62e362cdadfa15bf9e1a2fb1e
Sergei Vorontsov <sergei.a.vorontsov@gmail.com>
2024-10-06 15:40:21 -0700

    codacy fixes

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -19,9 +19,16 @@ export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     rules: {
       "react/react-in-jsx-scope": "off",
       "react/jsx-uses-react": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
     },
   },
 ]
