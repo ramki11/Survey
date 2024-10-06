3bbd73f3e01c81093a0b3954f0ecbcac7322330e
Sergei Vorontsov <sergei.a.vorontsov@gmail.com>
2024-10-06 09:24:49 -0700

    codacy fixes

--- frontend/src/components/Common/Table.tsx
+++ frontend/src/components/Common/Table.tsx
@@ -38,31 +38,30 @@ export function DataTable<Data extends object>({
         <Thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <Tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <Th key={header.id}>
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
-                    )}
-                  </Th>
-                )
-              })}
+              {headerGroup.headers.map((header) => (
+                <Th key={header.id}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext(),
+                  )}
+                </Th>
+              ))}
             </Tr>
           ))}
         </Thead>
         <Tbody>
-          {table.getRowModel().rows.map((row) => (
-            <Tr key={row.id} onClick={() => onRowClick?.(row.original)}>
-              {row.getVisibleCells().map((cell) => {
-                return (
+          {table.getRowModel().rows.map((row) => {
+            const { original } = row
+            return (
+              <Tr key={row.id} onClick={() => onRowClick?.(original)}>
+                {row.getVisibleCells().map((cell) => (
                   <Td key={cell.id}>
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </Td>
-                )
-              })}
-            </Tr>
-          ))}
+                ))}
+              </Tr>
+            )
+          })}
         </Tbody>
       </ChakraTable>
     </TableContainer>
