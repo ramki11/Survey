9a8e8570225db21feaf266fca07c71577b792a15
Jake Thrasher <mjakethrasher@gmail.com>
2024-10-10 20:15:58 -0500

    AddScheduledInquiry and logic

---
+++ frontend/src/components/ScheduledInquiries/AddScheduledInquiry.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@chakra-ui/react"
+import { useMutation } from "@tanstack/react-query"
+import { useState } from "react"
+import {
+  type ApiError,
+  type InquiryPublic,
+  ScheduledInquiriesService,
+  type ScheduledInquiryCreate,
+} from "../../client"
+import useCustomToast from "../../hooks/useCustomToast"
+import { handleError } from "../../utils/showToastOnError"
+import SimpleModal from "../Common/SimpleModal"
+
+type AddScheduledInquiryProps = {
+  inquiry: InquiryPublic
+}
+const AddScheduledInquiry = ({ inquiry }: AddScheduledInquiryProps) => {
+  const showToast = useCustomToast()
+  const [isModalOpen, setModalOpen] = useState(false)
+
+  const addToScheduledInquiries = async (data: ScheduledInquiryCreate) => {
+    return ScheduledInquiriesService.scheduledInquiriesAddToSchedule({
+      requestBody: data,
+    })
+  }
+
+  const mutation = useMutation({
+    mutationFn: addToScheduledInquiries,
+    onSuccess: () => {
+      showToast("Success!", "Added inquiry to schedule.", "success")
+      closeModal()
+    },
+    onError: (err: ApiError) => {
+      handleError(err, showToast)
+    },
+  })
+
+  const handleSubmit = () => {
+    mutation.mutate({ inquiry_id: inquiry.id })
+  }
+
+  const openModal = () => setModalOpen(true)
+  const closeModal = () => setModalOpen(false)
+
+  return (
+    <>
+      <Button onClick={openModal}>Add to Schedule</Button>
+      <SimpleModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        onSubmit={handleSubmit}
+        title={`You're about ot add this inquiry to the schedule. Are you sure?`}
+        content={<span>{inquiry.text}</span>}
+        submitButtonText="Continue"
+      />
+    </>
+  )
+}
+
+export default AddScheduledInquiry
