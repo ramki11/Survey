8a2a245512af69bfbed55de2239208b5194226b2
ramki11 <ratnakara.ramki@gmail.com>
2024-11-15 11:40:19 -0500

    Update ecr.tf
    Signed-off-by: ramki11 <ratnakara.ramki@gmail.com>

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -6,7 +6,4 @@ data "aws_ecr_repository" "instance" {
   name = aws_ecr_repository.instance.name
 }
 
-data "aws_ecr_image" "instance" {
-  repository_name = aws_ecr_repository.instance.name
-  image_tag       = "init"
 }
