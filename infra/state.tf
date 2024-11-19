33396dbadcad56454543ee4d4a576d477b01872a
ramki11 <ratnakara.ramki@gmail.com>
2024-11-19 17:27:47 -0500

    Update state.tf
    Signed-off-by: ramki11 <ratnakara.ramki@gmail.com>

--- infra/state.tf
+++ infra/state.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "survey-terraform-state-eu-west-2"
     key            = "survey/terraform.tfstate"
-    region         = "eu-west-2"
+    region         = "us-west-2"
     dynamodb_table = "survery-terraform-state-lock"
     encrypt        = true
   }
