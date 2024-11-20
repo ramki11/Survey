34e0721b4774062c394faab03ace161999956684
ramki11 <ratnakara.ramki@gmail.com>
2024-11-19 21:28:12 -0500

    Update main.tf
    Signed-off-by: ramki11 <ratnakara.ramki@gmail.com>

--- infra/main.tf
+++ infra/main.tf
@@ -25,26 +25,4 @@ module "iam" {
   source = "./modules/iam"
 }
 
-# Create application load balancer
-module "alb" {
-  source              = "./modules/alb"
-  public_subnets      = module.vpc.public_subnets
-  vpc_id              = module.vpc.vpc_id
-  project_name        = var.project_name
-  app_name            = var.app_name
-}
 
-# Create ecs cluster, service and task definition
-module "ecs" {
-  source                          = "./modules/ecs"
-  region                          = var.region
-  project_name                    = var.project_name
-  app_name                        = var.app_name
-  private_app_subnets             = module.vpc.aws_subnet.private_app
-  vpc_id                          = module.vpc.vpc_id
-  load_balancer_security_group_id = module.alb.alb_security_group_id
-  target_group_arn                = module.alb.target_group_arn
-  ecs_task_execution_role_arn     = module.iam.ecs_task_execution_role_arn
-  image_path                      = "913524926070.dkr.ecr.us-west-2.amazonaws.com/incredihire/backend:latest"
-  image_tag                       = var.image_tag
-}
