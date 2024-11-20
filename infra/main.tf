7f44abb78d2d1d501a2cc0dfe6192f75bfeb4ec9
ramki11 <ratnakara.ramki@gmail.com>
2024-11-19 20:28:13 -0500

    Update main.tf
    Signed-off-by: ramki11 <ratnakara.ramki@gmail.com>

--- infra/main.tf
+++ infra/main.tf
@@ -2,4 +2,19 @@ data "aws_caller_identity" "current" {}
 
 
 #hold
-#terraform init -reconfigure
+
+
+# Create ecs cluster, service and task definition
+module "ecs" {
+  source                          = "./modules/ecs"
+  region                          = var.region
+  project_name                    = var.project_name
+  app_name                        = var.app_name
+  private_app_subnets             = module.vpc.private_app_subnets
+  vpc_id                          = module.vpc.vpc_id
+  load_balancer_security_group_id = module.alb.alb_security_group_id
+  target_group_arn                = module.alb.target_group_arn
+  ecs_task_execution_role_arn     = module.iam.ecs_task_execution_role_arn
+  image_path                      = replace(replace(var.image_path, "{AWS-accountID}", data.aws_caller_identity.current.account_id), "{region}", var.region)
+  image_tag                       = var.image_tag
+}
