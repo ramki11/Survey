80ec08cbaf9a55143606bcbfd2589d8bc30dbe55
ramki11 <ratnakara.ramki@gmail.com>
2024-11-19 20:55:42 -0500

    Update main.tf
    Signed-off-by: ramki11 <ratnakara.ramki@gmail.com>

--- infra/main.tf
+++ infra/main.tf
@@ -2,19 +2,51 @@ data "aws_caller_identity" "current" {}
 
 
 #hold
+# create vpc
+module "vpc" {
+  source                        = "./modules/vpc"
+  region                        = var.region
+  project_name                  = var.project_name
+  vpc_data                      = var.vpc_data
+  ecs_service_security_group_id = module.ecs.ecs_service_security_group_id
+}
 
+# create nat gateway
+module "nat_gateway" {
+  source              = "./modules/nat-gateway"
+  vpc_id              = module.vpc.vpc_id
+  vpc_data            = var.vpc_data
+  public_subnets      = module.vpc.public_subnets
+  private_app_subnets = module.vpc.private_app_subnets
+  private_db_subnets  = module.vpc.private_db_subnets
+}
+
+# create iam role
+module "iam" {
+  source = "./modules/iam"
+}
+
+# Create application load balancer
+module "alb" {
+  source              = "./modules/alb"
+  public_subnets      = module.vpc.public_subnets
+  vpc_id              = module.vpc.vpc_id
+  project_name        = var.project_name
+  app_name            = var.app_name
+  acm_certificate_arn = module.acm.acm_certificate_arn
+}
 
 # Create ecs cluster, service and task definition
 module "ecs" {
   source                          = "./modules/ecs"
   region                          = var.region
   project_name                    = var.project_name
-  app_name                        = "backend-1"
+  app_name                        = var.app_name
   private_app_subnets             = module.vpc.public_app_subnets
   vpc_id                          = module.vpc.vpc_id
   load_balancer_security_group_id = module.alb.alb_security_group_id
   target_group_arn                = module.alb.target_group_arn
   ecs_task_execution_role_arn     = module.iam.ecs_task_execution_role_arn
-  image_path                      = replace(replace(var.image_path, "{AWS-accountID}", data.aws_caller_identity.current.account_id), "{region}", var.region)
+  image_path                      = "913524926070.dkr.ecr.us-west-2.amazonaws.com/incredihire/backend:latest"
   image_tag                       = var.image_tag
 }
