f7f6eef23016a0ad91a013f1b2502433580af231
ramki11 <ratnakara.ramki@gmail.com>
2024-12-06 14:20:03 -0500

    Update main.tf
    Signed-off-by: ramki11 <ratnakara.ramki@gmail.com>

--- infra/main.tf
+++ infra/main.tf
@@ -46,6 +46,6 @@ module "ecs" {
   load_balancer_security_group_id = module.alb.alb_security_group_id
   target_group_arn                = module.alb.target_group_arn
   ecs_task_execution_role_arn     = module.iam.ecs_task_execution_role_arn
-  image_path                      = replace(replace(var.image_path, "{AWS-accountID}", data.aws_caller_identity.current.account_id), "{region}", var.region)
+  image_path                      = replace(replace(var.image_path, "913524926070", data.aws_caller_identity.current.account_id), "{region}", var.region)
   image_tag                       = var.image_tag
 }
