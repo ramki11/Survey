650b87aacc811c07765b086922f3178fcb7b7bc0
ramki11 <ratnakara.ramki@gmail.com>
2024-12-06 18:12:44 -0500

    Update main.tf
    Signed-off-by: ramki11 <ratnakara.ramki@gmail.com>

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -49,6 +49,11 @@ resource "aws_alb_listener" "http" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
+default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
  # default_action {
  #   type = "redirect"
 #
