023a3898546b173ffbf84c7b91817c6b2388add5
ramki11 <ratnakara.ramki@gmail.com>
2024-12-06 14:19:23 -0500

    Update main.tf
    Signed-off-by: ramki11 <ratnakara.ramki@gmail.com>

--- infra/main.tf
+++ infra/main.tf
@@ -25,3 +25,27 @@ module "iam" {
 }
 
 
+# Create application load balancer
+module "alb" {
+  source              = "./modules/alb"
+  public_subnets      = module.vpc.public_subnets
+  vpc_id              = module.vpc.vpc_id
+  project_name        = var.project_name
+  app_name            = var.app_name
+  acm_certificate_arn = module.acm.acm_certificate_arn
+}
+
+# Create ecs cluster, service and task definition
+module "ecs" {
+  source                          = "./modules/ecs"
+  region                          = var.region
+  project_name                    = var.project_name
+  app_name                        = var.app_name
+  private_app_subnets             = module.vpc.private_app_subnets
+  vpc_id                          = module.vpc.vpc_id
+  load_balancer_security_group_id = module.alb.alb_security_group_id
+  target_group_arn                = module.alb.target_group_arn
+  ecs_task_execution_role_arn     = module.iam.ecs_task_execution_role_arn
+  image_path                      = replace(replace(var.image_path, "{AWS-accountID}", data.aws_caller_identity.current.account_id), "{region}", var.region)
+  image_tag                       = var.image_tag
+}
