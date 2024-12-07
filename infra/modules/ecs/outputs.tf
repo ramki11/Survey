97f0d1f734ba1de89fbce255c91f2a004ed46ce2
ramki11 <ratnakara.ramki@gmail.com>
2024-12-06 20:55:43 -0500

    Update outputs.tf
    Signed-off-by: ramki11 <ratnakara.ramki@gmail.com>

--- infra/modules/ecs/outputs.tf
+++ infra/modules/ecs/outputs.tf
@@ -2,3 +2,8 @@ output "ecs_service_security_group_id" {
   value       = aws_security_group.service.id
   description = "Security group ID for the ECS service created by this module"
 }
+
+
+output "container_definitions" {
+    value = file("./modules/ecs/backend-survey-task-definition.json")
+}
